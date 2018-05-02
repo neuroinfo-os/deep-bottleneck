c6c1e90c610d316dc901d2d438389b8a98c3fb04
Jonas <Jonas@jonas.de>
2018-05-02 18:01:57 +0200

    Include MI-computation

--- iclr_wrap_up/experiment.py
+++ iclr_wrap_up/experiment.py
@@ -2,8 +2,7 @@ import importlib
 
 from sacred import Experiment
 from sacred.observers import MongoObserver
-
-from tensorflow.python.keras.callbacks import Callback
+from .loggingreporter import LoggingReporter
 
 
 ex = Experiment('sacred_keras_example')
@@ -11,25 +10,20 @@ ex.observers.append(MongoObserver.create(url='mongodb://127.0.0.1:27017',
                                          db_name='dneck_test'))
 
 
-class Logger(Callback):
-
-    def __init__(self, run):
-        super().__init__()
-
-        self._run = run
-
-    def on_epoch_end(self, epoch, logs=None):
-        print(logs)
-        self._run.log_scalar("training.loss",  float(logs['loss']))
-
-
 @ex.config
 def hyperparameters():
-    epochs = 10
-    batch_size = 32
+    epochs = 10000
+    batch_size = 256
     architecture = [10, 7, 3]
+    learningrate = 0.0004
+    activation_fn = 'relu'
+    full_mi = True
+    architecture_name= '-'.join(map(str, architecture))
+    save_dir = 'rawdata/' + activation_fn + '_' + architecture_name
+    infoplane_measure = 'upper'
     model = 'models.feedforward'
     dataset = 'datasets.harmonics'
+    estimator = 'compute_mi.compute_mi_bi_net'
 
 @ex.capture
 def load_dataset(dataset):
@@ -41,16 +35,46 @@ def load_model(model, architecture):
     module = importlib.import_module(model)
     return module.load(architecture)
 
+
+def do_report(epoch):
+    # Only log activity for some epochs.  Mainly this is to make things run faster.
+    if epoch < 20:       # Log for all first 20 epochs
+        return True
+    elif epoch < 100:    # Then for every 5th epoch
+        return (epoch % 5 == 0)
+    elif epoch < 2000:    # Then every 10th
+        return (epoch % 20 == 0)
+    else:                # Then every 100th
+        return (epoch % 100 == 0)
+
+
 @ex.capture
-def make_callbacks(_run):
-    return [Logger(_run)]
+def make_callbacks(_run, training, test, full_mi, save_dir, batch_size, activation_fn):
+    callbacks = [LoggingReporter(trn=training, tst=test, full_mi=full_mi, save_dir=save_dir,
+                                 batch_size=batch_size, activation_fn= activation_fn, do_save_func=do_report)]
+    return callbacks
+
+@ex.capture
+def load_estimator(estimator, full_mi):
+    module = importlib.import_module(estimator)
+    cls = module.load()
+    return cls(full_mi)
 
 @ex.automain
 def conduct(epochs, batch_size):
-    dataset = load_dataset()
+    training, test = load_dataset()
     model = load_model()
-    callbacks = make_callbacks()
-    model.fit(dataset, dataset, epochs=epochs, batch_size=batch_size, callbacks=callbacks)
+    callbacks = make_callbacks(training, test)
+    model.fit(x=training.X, y=training.Y,
+                  verbose=2,
+                  batch_size=batch_size,
+                  epochs=epochs,
+                  # validation_data=(tst.X, tst.Y),
+                  callbacks=callbacks)
+    estimator = load_estimator()
+    estimator.compute_mi()
+
+
 
 
 
