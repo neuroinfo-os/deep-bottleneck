91076f698932e6e82cc45d6b4985cd3817f323e5
Chris Stenkamp <cstenkamp@uos.de>
2018-05-29 16:06:19 +0200

    equal binning with parameter 'bin2'

--- iclr_wrap_up/experiment.py
+++ iclr_wrap_up/experiment.py
@@ -24,7 +24,7 @@ def hyperparameters():
     architecture = [10, 7, 5, 4, 3]
     learning_rate = 0.0004
     full_mi = False
-    infoplane_measure = 'bin'
+    infoplane_measure = 'bin2'
     architecture_name = '-'.join(map(str, architecture))
     activation_fn = 'tanh'
     save_dir = 'rawdata/' + activation_fn + '_' + architecture_name
--- iclr_wrap_up/simplebinmi.py
+++ iclr_wrap_up/simplebinmi.py
@@ -1,5 +1,16 @@
 # Simplified MI computation code from https://github.com/ravidziv/IDNNs
 import numpy as np
+from functools import reduce, partial
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+
+def histedges_equalN(x, nbin):
+    bins = np.interp(np.linspace(0, len(x), nbin + 1), np.arange(len(x)), np.sort(x))
+    err = reduce(lambda a, b: -1 if ((a == b) or (a == -1)) else b, bins) == -1
+    if err:
+        print("I cannot do a good binning, one unique value appears too often!")
+    return bins
+
 
 
 def get_unique_probs(x):
@@ -12,6 +23,28 @@ def bin_calc_information2(labelixs, layerdata, binsize):
     # This is even further simplified, where we use np.floor instead of digitize
     def get_h(d):
         digitized = np.floor(d / binsize).astype('int')
+        p_ts, _ = get_unique_probs(digitized)
+        return -np.sum(p_ts * np.log(p_ts))
+
+    H_LAYER = get_h(layerdata)
+    H_LAYER_GIVEN_OUTPUT = 0
+    for label, ixs in labelixs.items():
+        H_LAYER_GIVEN_OUTPUT += ixs.mean() * get_h(layerdata[ixs, :])
+    return H_LAYER, H_LAYER - H_LAYER_GIVEN_OUTPUT
+
+
+def bin_calc_information_evenbins(labelixs, layerdata, numbins):
+    # This is even further simplified, where we use np.floor instead of digitize
+    def get_h(d):
+        def get_bin(x, bins):
+            for num, bin in enumerate(bins):
+                if x < bin:
+                    return num
+        digitize = partial(get_bin, bins=histedges_equalN(flatten(d), numbins))
+        digitized = []
+        for line in d:
+            l2 = [digitize(cell) for cell in line]
+            digitized.append(l2)
         p_ts, _ = get_unique_probs(digitized)
         return -np.sum(p_ts * np.log(p_ts))
 
