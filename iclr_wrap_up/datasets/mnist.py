f1cb15cb44bd82a567a569e25d5f25169ec3d68a
pphilihpp <phil.fu@web.de>
2018-06-19 17:17:21 +0200

    Refactor variable names

--- iclr_wrap_up/datasets/mnist.py
+++ iclr_wrap_up/datasets/mnist.py
@@ -15,7 +15,7 @@ def load():
         - y is class, with numbers from 0 to 9
         - Y is class, but coded as a 10-dim vector with one entry set to 1 at the column index corresponding to the class
     """
-    n_classes = 10
+    nb_classes = 10
     (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
     X_train = np.reshape(X_train, [X_train.shape[0], -1]).astype('float32') / 255.0
     X_test = np.reshape(X_test, [X_test.shape[0], -1]).astype('float32') / 255.0
@@ -23,11 +23,11 @@ def load():
     X_train = X_train * 2.0 - 1.0
     X_test = X_test * 2.0 - 1.0
 
-    Y_train = keras_utils.to_categorical(y_train, n_classes).astype('float32')
-    Y_test = keras_utils.to_categorical(y_test, n_classes).astype('float32')
+    Y_train = keras_utils.to_categorical(y_train, nb_classes).astype('float32')
+    Y_test = keras_utils.to_categorical(y_test, nb_classes).astype('float32')
 
-    Dataset = namedtuple('Dataset', ['X', 'Y', 'y', 'n_classes'])
-    training = Dataset(X_train, Y_train, y_train, n_classes)
-    test = Dataset(X_test, Y_test, y_test, n_classes)
+    Dataset = namedtuple('Dataset', ['X', 'Y', 'y', 'nb_classes'])
+    training = Dataset(X_train, Y_train, y_train, nb_classes)
+    test = Dataset(X_test, Y_test, y_test, nb_classes)
 
     return training, test
