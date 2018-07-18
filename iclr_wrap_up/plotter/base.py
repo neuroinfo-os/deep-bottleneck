07c4672f8193fad45cdac233d1e82e414aed27e7
felixmzd <post@fmzd.de>
2018-07-18 14:24:27 +0200

    Feature/plots (#125)
    * Fix SNR plot for 1 layer architecture
    
    * Show epoch number in infoplane movie
    
    * Readjust frequency of saved epoch activations
    
    * Test activation patterns with re-normalizing weights
    
    * Set max_norm_weights as experiment parameter
    
    * Change keras import
    
    * Improve parameter handling
    
    * Add plot for activations of all neurons over epochs
    
    * Experiment with modifications to entropy estimation
    
    * Fix parameter layer size
    
    * Refactor plotting code
    
    * Fix first layer neuron count
    
    * Include review fixes

--- iclr_wrap_up/plotter/base.py
+++ iclr_wrap_up/plotter/base.py
@@ -8,7 +8,7 @@ class BasePlotter:
     def generate(self, measures_summary):
         fig = self.plot(measures_summary)
         filename = f'plots/{self.plotname}.png'
-        fig.savefig(filename, bbox_inches='tight', dpi=600)
+        fig.savefig(filename, bbox_inches='tight', dpi=300)
 
         self.run.add_artifact(filename, name=self.plotname)
 
