Fix min max calculation for histograms

iclr_wrap_up/plotter/activations.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from iclr_wrap_up import utils
+
 from iclr_wrap_up.plotter.base import BasePlotter
 
 
@@ -16,6 +18,7 @@ class ActivityPlotter(BasePlotter):
         self.dataset = dataset
         self.run = run
 
+
     def plot(self, measures_summary):
         activations_summary = measures_summary['activations_summary']
         num_layers = len(activations_summary[0]['weights_norm'])  # get number of layers indirectly via number of values
@@ -28,9 +31,12 @@ class ActivityPlotter(BasePlotter):
         for layer in range(num_layers):
             ax = fig.add_subplot(num_layers, 1, layer + 1)
 
+            min, max = utils.get_min_max(all_activations, layer_number=layer)
+            bins = np.linspace(min, max, 30)
+
             hist = []
-            for epoch, epoch_values in all_activations.items():
-                hist.append(np.histogram(epoch_values[layer], bins=30)[0])
+            for epoch, activations in all_activations.items():
+                hist.append(np.histogram(activations[layer], bins=bins)[0])
 
             hist_df = pd.DataFrame(hist)
 

iclr_wrap_up/plotter/activations_single_neuron.py
@@ -3,7 +3,9 @@ import matplotlib.gridspec as gridspec
 import numpy as np
 import pandas as pd
 
+
 from iclr_wrap_up.plotter.base import BasePlotter
+from iclr_wrap_up import utils
 
 
 def load(run, dataset):
@@ -35,7 +37,10 @@ class SingleNeuronActivityPlotter(BasePlotter):
         for epoch, activations in all_activations.items():
             layer_activations = activations[layer_number].transpose()
             for neuron_number in range(neurons_in_layer):
-                histogram_per_neuron, _ = np.histogram(layer_activations[neuron_number], bins=30)
+                min, max = utils.get_min_max(all_activations, layer_number, neuron_number)
+                bins = np.linspace(min, max, 30)
+
+                histogram_per_neuron, _ = np.histogram(layer_activations[neuron_number], bins=bins)
                 hist[neuron_number].append(histogram_per_neuron)
         return hist
 

iclr_wrap_up/utils.py
@@ -17,7 +17,7 @@ def construct_full_dataset(training, test):
     Returns:
         A new Namedtuple with fields X, y and Y containing the concatenation of training and test data
     """
-    Dataset = namedtuple('Dataset',['X','Y','y','n_classes'])
+    Dataset = namedtuple('Dataset', ['X', 'Y', 'y', 'n_classes'])
     X = np.concatenate((training.X, test.X))
     y = np.concatenate((training.y, test.y))
     Y = np.concatenate((training.Y, test.Y))
@@ -70,3 +70,24 @@ def is_dense_like(layer):
         True if layer has attribute 'kernel', False otherwise
     """
     return hasattr(layer, 'kernel')
+
+
+def get_min_max(activations, layer_number, neuron_number=None):
+    total_max = 0
+    total_min = 0
+    for epoch, activations in activations.items():
+
+        if neuron_number is not None:
+            layer_activations = activations[layer_number].transpose()
+            current_max = np.max(layer_activations[neuron_number])
+            current_min = np.min(layer_activations[neuron_number])
+        else:
+            current_max = np.max(activations[layer_number])
+            current_min = np.min(activations[layer_number])
+
+        if current_max > total_max:
+            total_max = current_max
+        if current_min < total_min:
+            total_min = current_min
+
+    return total_min, total_max