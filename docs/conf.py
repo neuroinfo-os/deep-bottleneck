13a678d31fa63fdce59d3b535e551ecc0ff2d1db
JarnoRFB <ruedigerbusche@web.de>
2018-08-15 16:53:12 +0200

    Mock tf import

--- docs/conf.py
+++ docs/conf.py
@@ -176,4 +176,4 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-autodoc_mock_imports = ['_tkinter']
+autodoc_mock_imports = ['_tkinter', 'tensorflow']
