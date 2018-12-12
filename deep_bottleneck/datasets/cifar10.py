5d0cf9f61886ea4bd388aba30f60ea85739aced1
pphilihpp <phil.fu@web.de>
2018-12-12 13:11:50 +0100

    cifar10 dataset included

--- deep_bottleneck/datasets/cifar10.py
+++ deep_bottleneck/datasets/cifar10.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from tensorflow import keras
 from tensorflow.python.keras import utils as keras_utils
+from deep_bottleneck.datasets.base_dataset import Dataset
 
 
 def load():
@@ -16,19 +17,17 @@ def load():
         - y is class, with numbers from 0 to 9
         - Y is class, but coded as a 10-dim vector with one entry set to 1 at the column index corresponding to the class
     """
-    nb_classes = 10
+    n_classes = 10
     (X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
     X_train = np.reshape(X_train, [X_train.shape[0], -1]).astype('float32') / 255.0
     X_test = np.reshape(X_test, [X_test.shape[0], -1]).astype('float32') / 255.0
 
+    y_train = np.squeeze(y_train)
+    y_test = np.squeeze(y_test)
+
     X_train = X_train * 2.0 - 1.0
     X_test = X_test * 2.0 - 1.0
 
-    Y_train = keras_utils.to_categorical(y_train, nb_classes).astype('float32')
-    Y_test = keras_utils.to_categorical(y_test, nb_classes).astype('float32')
-
-    Dataset = namedtuple('Dataset', ['X', 'Y', 'y', 'nb_classes'])
-    training = Dataset(X_train, Y_train, y_train, nb_classes)
-    test = Dataset(X_test, Y_test, y_test, nb_classes)
-
-    return training, test
+    dataset = Dataset.from_labelled_subset(X_train, y_train, X_test, y_test, n_classes)
+    
+    return dataset
